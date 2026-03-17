3c5cbf6b6fcf4b5a703423bdcb469038c3574081
Amira Maria Ayoub <aayo@itu.dk>
2026-03-17 14:05:56 +0100

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
+
 import * as vscode from 'vscode';
-import { SidebarProvider } from "./SidebarProvider";
+import { SidebarProvider } from "./sidebarProvider";
 
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const sidebarProvider = new SidebarProvider(context.extensionUri);
 	context.subscriptions.push(
@@ -15,10 +13,8 @@ export function activate(context: vscode.ExtensionContext) {
 		));
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('carrot.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Carrot!');
 	});
@@ -69,7 +65,7 @@ export function activate(context: vscode.ExtensionContext) {
 			const decorationType = vscode.window.createTextEditorDecorationType({
 				gutterIconPath : decoPath,
 				gutterIconSize : "contain",
-				//isWholeLine : true
+				isWholeLine : true
 			});
 			
 			//Create empty deco options
