4e79e26094fb9cffb5ee3eca3f9fae81b86264ed
Amira Maria Ayoub <aayo@itu.dk>
2026-03-24 17:24:53 +0100

    boilerplate from vscode

--- src/Note.ts
+++ src/Note.ts
@@ -1,24 +1,44 @@
 import * as vscode from 'vscode';
 
 export class Note {
-
     private readonly id : number;
     private readonly commentId : number;
+    public static currentPanel: Note | undefined;
+    public static readonly viewType = 'carrotNote';
 
-    constructor(id: number, commentId: number){
+    private readonly _panel: vscode.WebviewPanel;
+    private readonly _extensionUri: vscode.Uri;
+    private _disposables: vscode.Disposable[] = [];
+
+    constructor(id: number, commentId: number, panel: vscode.WebviewPanel, extensionUri: vscode.Uri){
         this.id = id;
         this.commentId = commentId;
+        this._panel = panel;
+        this._extensionUri = extensionUri;
     }
 
 
-    createPanel(context: vscode.ExtensionContext) : vscode.WebviewPanel {
+    public static createOrShow(extensionUri: vscode.Uri) {
+        // Creates a column based on the text editor. Checks for null.
+        const column = vscode.window.activeTextEditor
+			? vscode.window.activeTextEditor.viewColumn
+			: undefined;
+
+        // If we already made a panel, shwo it, because we are limited to one panel.
+        if (Note.currentPanel) {
+            Note.currentPanel._panel.reveal(column);
+            return;
+        }
+
+        // otherwise, create new panel.
         const panel = vscode.window.createWebviewPanel(
-            'carrotNote', // Identifies the type of the webview. Used internally
+            Note.viewType, // Identifies the type of the webview. 
             'New Carrot Note', // Title of the panel displayed to the user
-            vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+            column || vscode.ViewColumn.One, // Editor column to show the new webview panel in.
             {} // Webview options. More on these later.
         );
-
-        return panel;
+        
+        // Set the panel.
+        Note.currentPanel = new Note(1, 1, panel, extensionUri);
     }
 }
