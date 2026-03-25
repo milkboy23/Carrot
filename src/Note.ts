99dc56dcb23dba2cdb8cb2ebce1b932317e315e9
Amira Maria Ayoub <aayo@itu.dk>
2026-03-25 10:56:33 +0100

    added VSCode webview foundation
    Co-Authored-By: EmmaAndrea <54407756+EmmaAndrea@users.noreply.github.com>
    Co-Authored-By: Um-Kulsum Abusheva <umkulsum2630@gmail.com>

--- src/Note.ts
+++ src/Note.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { getNonce } from './getNonce';
 
 export class Note {
     private readonly id : number;
@@ -15,6 +16,58 @@ export class Note {
         this.commentId = commentId;
         this._panel = panel;
         this._extensionUri = extensionUri;
+
+        
+        this._update();
+
+		// Listen for when the panel is disposed
+		// This happens when the user closes the panel or when the panel is closed programmatically
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+		// Update the content based on view changes
+		this._panel.onDidChangeViewState(
+			() => {
+				if (this._panel.visible) {
+					this._update();
+				}
+			},
+			null,
+			this._disposables
+		);
+
+		// Handle messages from the webview
+		this._panel.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'alert':
+						vscode.window.showErrorMessage(message.text);
+						return;
+				}
+			},
+			null,
+			this._disposables
+		);
+	}
+
+	public doRefactor() {
+		// Send a message to the webview webview.
+		// You can send any JSON serializable data.
+		this._panel.webview.postMessage({ command: 'refactor' });
+	}
+
+	public dispose() {
+		Note.currentPanel = undefined;
+
+		// Clean up our resources
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x) {
+				x.dispose();
+			}
+		}
+	
     }
 
 
@@ -35,10 +88,68 @@ export class Note {
             Note.viewType, // Identifies the type of the webview. 
             'New Carrot Note', // Title of the panel displayed to the user
             column || vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-            {} // Webview options. More on these later.
+            {
+                enableScripts: true,
+                localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
+            } 
         );
         
         // Set the panel.
         Note.currentPanel = new Note(1, 1, panel, extensionUri);
     }
+
+    public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		Note.currentPanel = new Note(1, 1, panel, extensionUri);
+	}
+
+    private _update(){
+        const webview = this._panel.webview;
+
+        this._panel.title = "New Carrot Note";
+		this._panel.webview.html = this._getHtmlForWebview(webview);
+    }
+
+   	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Local path to main script run in the webview
+		const scriptPathOnDisk = vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js');
+
+		// And the uri we use to load this script in the webview
+		const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
+
+		// Local path to css styles
+		const styleResetPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css');
+		const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css');
+
+		// Uri to load styles into webview
+		const stylesResetUri = webview.asWebviewUri(styleResetPath);
+		const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+
+		// Use a nonce to only allow specific scripts to be run
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${stylesResetUri}" rel="stylesheet">
+				<link href="${stylesMainUri}" rel="stylesheet">
+
+				<title>Cat Coding</title>
+			</head>
+			<body>
+				<h1 id="lines-of-code-counter">0</h1>
+
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+	}
 }
