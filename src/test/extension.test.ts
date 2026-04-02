53d43ab0b5cf966df0c01a9207bd85c8727930fc
Amira Maria Ayoub <aayo@itu.dk>
2026-04-02 14:25:12 +0200

    CommentManager unit tests done
    Co-Authored-By: EmmaAndrea <54407756+EmmaAndrea@users.noreply.github.com>
    Co-Authored-By: Um-Kulsum Abusheva <umkulsum2630@gmail.com>

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,12 +3,18 @@ import * as vscode from 'vscode';
 import { createMockContext, MockState } from './MockState';
 import * as myExtension from '../extension';
 import { CommentManager } from '../CommentManager';
+import { SerializedComment } from '../SerializedComment';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
 	// Creating a mock context from our MockState.ts
 	let fakeContext = createMockContext();
+	let cm : CommentManager;
+	let noteId : number;
+	let uri : vscode.Uri;
+	let start : number;
+	let hoverMessage : string;
 
 	suiteTeardown(() => {
 		vscode.window.showInformationMessage('All tests done!');
@@ -21,19 +27,50 @@ suite('Extension Test Suite', () => {
 		} else {
 			vscode.window.showInformationMessage('All tests done!');
 		}
-		
+
+
+		// Setting up a mock CommentManager and a mock Comment
+		cm = CommentManager.getInstance(fakeContext.workspaceState);
+		noteId = 0;
+		uri = "src/test/workspace/testWorkspaceFile.ts" as unknown as vscode.Uri;
+		start = 0;
+		hoverMessage = "Testing Carrot <3";
 	});
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
 
-	// Test that there is only one instance of the CommentManager
-	test('Test CommentManager Singleton', () => {
+	// CommentManager tests
+
+	test('Test CommentManager is a Singleton', () => {
 		const instance1 = CommentManager.getInstance(fakeContext.workspaceState);
 		const instance2 = CommentManager.getInstance(fakeContext.workspaceState);
 
 		assert.strictEqual(instance1, instance2);
 	});
+
+	test('Add comment through CommentManager', async () => {
+		await cm.addComment(noteId, uri, start, hoverMessage);
+		const commentList = fakeContext.workspaceState.get<SerializedComment[]>("comments", []);
+
+		assert.equal(commentList.length, 1);
+	});
+
+	test('Check commentId', () => {
+		const commentList = fakeContext.workspaceState.get<SerializedComment[]>("comments", []);
+		const comment = commentList.pop();
+		const commentId = comment?.id;
+
+		assert.equal(commentId, 0);
+	});
+
+	test('Delete comment', async () => {
+		await cm.addComment(noteId, uri, start, hoverMessage);
+		const comment = {id: 1, noteId: noteId, editorUri: uri.toString(), start: start, hoverMessage: hoverMessage};
+		await cm.deleteComments([comment]);
+		const commentList = fakeContext.workspaceState.get<SerializedComment[]>("comments", []);
+
+		assert.equal(commentList.length, 0);
+	});
+
+
+	// NoteManager tests
 });
